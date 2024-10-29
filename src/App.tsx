6cceb97e45895d166701a7a96ed001ea80d36dc7
anryli <anryhasegawalee@163.com>
2024-10-29 19:24:30 +0900

    conversation_box

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import "./App.css";
 import { SettingButton } from "./components/SettingButton/SettingButton";
 // import { NextButton } from "./components/NextButton/NextButton";
 import { SelectButton } from "./components/SelectButton/SelectButton";
+import { Conversation } from "./components/Conversation/Conversation";
+
 function App() {
   return (
     <div className="w-screen h-screen border-x-[32px] border-y-[28px] border-black">
@@ -15,7 +17,10 @@ function App() {
         <div className="row-[span_7_/_span_7] col-[span_27_/_span_27] border-b-[1px] border-orange">
           <SettingButton />
         </div>
-        <div className="row-[span_71_/_span_71] col-[span_100_/_span_100] border-b-[1px] border-orange"></div>
+        <div className="row-[span_50_/_span_50] col-[span_100_/_span_100] border-b-[1px] border-transparent"></div>
+        <div className="flex items-center justify-center row-[span_21_/_span_21] col-[span_100_/span_100] border-b-[1px] border-orange">
+        <Conversation/>
+        </div>
         <div className="row-[span_22_/_span_22] col-[span_67_/_span_67] border-r-[1px] border-orange">
           <div className="flex flex-col justify-around w-full h-full ">
             <span className="flex justify-around leading-none font-dot text-[1.75rem] leading-[1.5] text-beige">
