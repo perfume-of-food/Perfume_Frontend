ef34c37791a4fad08e40777fec10f5258f0d4a0e
TamagawaJousui <118496913+TamagawaJousui@users.noreply.github.com>
2024-10-31 17:17:27 +0900

    change home postion

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,22 +1,21 @@
 import { HomeButton } from "../../components/HomeButton/HomeButton";
 
-export function Home(){
-    const backgroundStyle = {
-        backgroundImage: "url('/src/statics/home_background.png')",
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        backgroundRepeat: "no-repeat"
-    };
-    return(
-        //最外层
-             <div
-            className="w-screen h-screen bg-black"
-            style={backgroundStyle}
-             >
-                <div className="flex justify-center items-center pt-[843px] pb-[300px] space-y-[50px]">
-                        <HomeButton />
-                </div>
-            </div>
-
-    );
+export function Home() {
+  const backgroundStyle = {
+    backgroundImage: "url('/src/statics/home_background.png')",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+  };
+  return (
+    // 最外层
+    <div
+      style={backgroundStyle}
+      className="w-screen h-screen bg-black relative"
+    >
+      <div className="flex w-full justify-center absolute top-[62vh]">
+        <HomeButton />
+      </div>
+    </div>
+  );
 }
