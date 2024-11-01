86a07bfce15f8d60a8524dc10f4ddca7d23d459e
TamagawaJousui <118496913+TamagawaJousui@users.noreply.github.com>
2024-11-02 06:46:55 +0900

    add MoodSelection dummy

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import "./App.css";
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import { Cover } from "@/pages/Cover/Cover";
 import { Intro } from "@/pages/Intro/Intro";
-import { MoodDashboard } from "@/pages/MoodDashboard/MoodDashboard";
+import { MoodSelection } from "@/pages/MoodSelection/MoodSelection";
 
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Cover />} />
         <Route path="/intro" element={<Intro />} />
-        <Route path="/mood-dashboard" element={<MoodDashboard />} />
+        <Route path="/mood-selection" element={<MoodSelection />} />
       </Routes>
     </Router>
   );
--- src/pages/Intro/Intro.tsx
+++ src/pages/Intro/Intro.tsx
@@ -66,7 +66,7 @@ export function Intro() {
           }}
           onConfirm={() => {
             console.log(userName);
-            navigate("/mood-dashboard");
+            navigate("/mood-selection");
           }}
         />
       )}
