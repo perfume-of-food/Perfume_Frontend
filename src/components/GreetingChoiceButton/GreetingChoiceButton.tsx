3be0d9b4f1e6b5fdf62a38aef7b4c77fdc2c8602
anryli <anryhasegawalee@163.com>
2024-11-02 09:12:17 +0900

    Button_UI_change

--- src/components/GreetingChoiceButton/GreetingChoiceButton.tsx
+++ src/components/GreetingChoiceButton/GreetingChoiceButton.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 interface GreetingChoiceButtonProps {
   onClick: () => void;
 }
@@ -5,11 +7,18 @@ interface GreetingChoiceButtonProps {
 export const GreetingChoiceButton = ({
   onClick,
 }: GreetingChoiceButtonProps) => {
+  const [isStartPressed, setIsStartPressed] = useState(false);
+  const [isUsagePressed, setIsUsagePressed] = useState(false);
+
   return (
     <div className="flex flex-col w-full h-full justify-around items-center p-[2px] relative">
       <div
         className="w-[70%] h-[30%] flex justify-center items-center relative group"
         onClick={onClick}
+        onMouseDown={() => setIsStartPressed(true)}
+        onMouseUp={() => setIsStartPressed(false)}
+        onTouchStart={() => setIsStartPressed(true)}
+        onTouchEnd={() => setIsStartPressed(false)}
       >
         <svg
           className="w-full h-full"
@@ -21,7 +30,7 @@ export const GreetingChoiceButton = ({
           <path
             d="M5.18924 5.38553L9.86979 0.5H237.13L246.5 10.2801V76.7199L241.811 81.6145L237.13 86.5H9.86979L5.18924 81.6145L0.5 76.7199V10.2801L5.18924 5.38553Z"
             fill="#FEA237"
-            className="[fill-opacity:0.2] [stroke-opacity:1] group-active:[fill-opacity:0.5]"
+            style={{ fillOpacity: isStartPressed ? 0.5 : 0.2, strokeOpacity: 1 }}
             stroke="#FEA237"
           />
         </svg>
@@ -33,6 +42,10 @@ export const GreetingChoiceButton = ({
       <div
         className="w-[70%] h-[30%] flex justify-center items-center relative group"
         onClick={onClick}
+        onMouseDown={() => setIsUsagePressed(true)}
+        onMouseUp={() => setIsUsagePressed(false)}
+        onTouchStart={() => setIsUsagePressed(true)}
+        onTouchEnd={() => setIsUsagePressed(false)}
       >
         <svg
           className="w-full h-full"
@@ -44,7 +57,7 @@ export const GreetingChoiceButton = ({
           <path
             d="M5.18924 5.38553L9.86979 0.5H237.13L246.5 10.2801V76.7199L241.811 81.6145L237.13 86.5H9.86979L5.18924 81.6145L0.5 76.7199V10.2801L5.18924 5.38553Z"
             fill="#FEA237"
-            className="[fill-opacity:0.2] [stroke-opacity:1] group-active:[fill-opacity:0.5]"
+            style={{ fillOpacity: isUsagePressed ? 0.5 : 0.2, strokeOpacity: 1 }}
             stroke="#FEA237"
           />
         </svg>
--- src/components/HomeButton/HomeButton.tsx
+++ src/components/HomeButton/HomeButton.tsx
@@ -1,14 +1,22 @@
 import "./HomeButton.css";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
 
 export const HomeButton = () => {
   const navigate = useNavigate();
+  const [isStartPressed, setIsStartPressed] = useState(false);
+  const [isUsagePressed, setIsUsagePressed] = useState(false);
 
     return(
      <div className="flex flex-col w-full h-full justify-around items-center p-[2px] relative space-y-[50px]">
        <div className="w-[247px] h-[87px] flex justify-center items-center relative"
             onClick={()=>navigate("/intro")}
             style = {{cursor:"pointer"}}
+            onMouseDown={() => setIsStartPressed(true)}
+            onMouseUp={() => setIsStartPressed(false)}
+            onTouchStart={() => setIsStartPressed(true)}
+            onTouchEnd={() => setIsStartPressed(false)}
        >
         <svg
           className="w-full h-full"
@@ -20,7 +28,7 @@ export const HomeButton = () => {
           <path
             d="M5.18924 5.38553L9.86979 0.5H237.13L246.5 10.2801V76.7199L241.811 81.6145L237.13 86.5H9.86979L5.18924 81.6145L0.5 76.7199V10.2801L5.18924 5.38553Z"
             fill="#FEA237"
-            fill-opacity="0.2"
+            style={{ fillOpacity: isStartPressed ? 0.5 : 0.2, strokeOpacity: 1 }}
             stroke="#FEA237"
           />
         </svg>
@@ -30,7 +38,12 @@ export const HomeButton = () => {
         </span>
 
         </div>
-        <div className="w-[247px] h-[87px] flex justify-center items-center relative">
+        <div className="w-[247px] h-[87px] flex justify-center items-center relative"
+        onMouseDown={() => setIsUsagePressed(true)}
+        onMouseUp={() => setIsUsagePressed(false)}
+        onTouchStart={() => setIsUsagePressed(true)}
+        onTouchEnd={() => setIsUsagePressed(false)}
+        >
         <svg
           className="w-full h-full"
           preserveAspectRatio="none"
@@ -41,7 +54,7 @@ export const HomeButton = () => {
           <path
             d="M5.18924 5.38553L9.86979 0.5H237.13L246.5 10.2801V76.7199L241.811 81.6145L237.13 86.5H9.86979L5.18924 81.6145L0.5 76.7199V10.2801L5.18924 5.38553Z"
             fill="#FEA237"
-            fill-opacity="0.2"
+            style={{ fillOpacity: isUsagePressed ? 0.5 : 0.2, strokeOpacity: 1 }}
             stroke="#FEA237"
           />
         </svg>
