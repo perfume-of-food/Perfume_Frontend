91bd38c1b031f30902f632e034c0a6d76430ec42
TamagawaJousui <118496913+TamagawaJousui@users.noreply.github.com>
2024-11-04 03:13:29 +0900

    change food desc

--- src/components/FoodDescription/FoodDescription.tsx
+++ src/components/FoodDescription/FoodDescription.tsx
@@ -4,7 +4,7 @@ export const FoodDescription = () => {
   const { selectedFood, isDescriptionFlashing } = useMoodMenuStore();
   const description = selectedFood?.description;
   return (
-    <div className="flex center items-center p-12 text-[#FFEFD0] text-3xl font-dot h-full w-full">
+    <div className="flex center items-center p-2 text-[#FFEFD0] text-3xl font-dot h-full w-full">
       <span
         className="px-12"
         style={{
@@ -13,7 +13,9 @@ export const FoodDescription = () => {
             : "none",
         }}
       >
-        {description ?? "アイコンをタップして、好きな食べ物を選んでください"}
+        {selectedFood
+          ? `「${selectedFood.title}」：${description}`
+          : "アイコンをタップして、好きな食べ物を選んでください"}
       </span>
     </div>
   );
