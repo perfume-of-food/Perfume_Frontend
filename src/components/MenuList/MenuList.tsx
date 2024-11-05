b6629b17356aed719f41e00bb0d69a328b4b47c8
TamagawaJousui <118496913+TamagawaJousui@users.noreply.github.com>
2024-11-06 01:06:45 +0900

    change vinegar, padding of menulist

--- src/components/MenuList/MenuList.tsx
+++ src/components/MenuList/MenuList.tsx
@@ -87,7 +87,7 @@ const MenuList = () => {
 
       <div
         ref={containerRef}
-        className="relative px-5 py-2 overflow-y-scroll h-full hidden-scrollbar"
+        className="relative pl-5 pr-6 py-2 overflow-y-scroll h-full hidden-scrollbar"
       >
         {displayFoods.map((food) => (
           <div
