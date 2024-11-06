7c74f73bdbfee4299fe0b4aa80ee573a79c26320
TamagawaJousui <118496913+TamagawaJousui@users.noreply.github.com>
2024-11-06 20:05:35 +0900

    convert png to webp

--- src/pages/Cover/Cover.tsx
+++ src/pages/Cover/Cover.tsx
@@ -2,7 +2,7 @@ import { HomeMenuButton } from "@/components/HomeMenuButton/HomeMenuButton";
 
 export function Cover() {
   return (
-    <div className="w-screen h-screen bg-black relative bg-[url('/home_background.png')] bg-cover bg-center bg-no-repeat">
+    <div className="w-screen h-screen bg-black relative bg-[url('/home_background.webp')] bg-cover bg-center bg-no-repeat">
       <div className="flex w-full justify-center absolute top-[62vh]">
         <HomeMenuButton />
       </div>
--- src/pages/Survey/Survey.tsx
+++ src/pages/Survey/Survey.tsx
@@ -2,7 +2,7 @@ import { SettingButton } from "@/components/SettingButton/SettingButton";
 
 export function Survey() {
   return (
-    <div className="w-screen h-screen bg-black relative bg-[url('/end_background.png')] bg-cover bg-center bg-no-repeat">
+    <div className="w-screen h-screen bg-black relative bg-[url('/end_background.webp')] bg-cover bg-center bg-no-repeat">
       <div className="flex w-full justify-center items-center pt-[71vh] ">
         <div className="flex w-[250px] h-[90px]">
           <SettingButton />
